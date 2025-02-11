e916d10cfbf568e923021f3a2a350ca8fdf7a4d4
nikhil-chigali-lv <nikhil.chigali@latentview.com>
2025-02-11 14:56:14 -0600

    Checkpointing extract_from_csv file

---
+++ feature_engineering/extract_from_csv.py
@@ -0,0 +1,81 @@
+# Data folder structure
+# ├── data/
+# │   ├── hbt/
+# │   |   ├── hbt.nodes_wifi_hbt.csv
+# │   |   ├── hbt.nodes_wan_hbt.csv
+# │   |   ├── hbt.nodes_stations_hbt.csv
+# │   |   ├── hbt.group_nodes_hbt.csv
+# │   ├── diag/
+# │   |   ├── diag.nodes_speedtest_diag.csv
+# │   |   ├── diag.nodes_group_wifi_scan_diag.csv
+# │   |   ├── diag.nodes_acs_channelchange_diag.csv
+# │   ├── evt/
+# │   |   ├── evt.nodes_station_evt.csv
+# │   |   ├── evt.stations_mesh_evt.csv
+# │   |   ├── evt.nodes_events_evt.csv
+
+import os
+import sys
+import numpy as np
+import pandas as pd
+from datetime import datetime
+
+from pathlib import Path
+from loguru import logger
+
+sys.path.append(os.path.join(os.path.dirname(__file__), ".."))
+from constants import DATA_DIR
+
+
+def load_data(data_path: Path):
+    """
+    Load data from a CSV file
+    :param data_path: Path to the CSV file
+    :return: DataFrame
+    """
+    return pd.read_csv(data_path)
+
+
+def extract_data(device_id: str, startdate: str):
+    # Features to extract
+    # Data used - upload ✅
+    #   hbt.nodes_stations_hbt.stations_bs
+    # Data used - download ✅
+    #   hbt.nodes_stations_hbt.stations_br
+    # Speed - upload ✅
+    #   diag.nodes_speedtest_diag.ookla_result.UploadSpeed
+    # Speed - download ✅
+    #   diag.nodes_speedtest_diag.ookla_result.DownloadSpeed
+    # Legacy devices exist ✅
+    #   if hbt.nodes_stations_hbt.stations_mode in [0,1,2,3] then "Yes" else "No"
+    # Signal Strength ✅
+    #   hbt.nodes_stations_hbt.stations_signal_strength
+    # Stationary vs Mobile ✅ ❓
+    #   Compute "90th Percentile" and "Median" of `Signal Strength`
+    #   If the difference between the two is greater than 5, then "Mobile" else "Stationary"
+    # Phy Rate {DownlinkPhyRate, MaxDownlinkPhyRate, UplinkPhyRate, MaxUplinkPhyRate} ✅
+    #   hbt.nodes_stations_hbt.[stations_tx_link_rate, stations_max_tx_phy_rate, stations_link_rate, stations_max_rx_phy_rate]
+
+    raw_data = {}
+    try:
+        startdate = datetime.strptime(startdate, "%Y-%m-%d")
+    except ValueError:
+        logger.error("Invalid start date format. Use 'YYYY-MM-DD' format")
+        sys.exit(1)
+
+    # Data Used (UL, DL), Legacy Devices Exist, Signal Strength, Phy Rate
+    filename = DATA_DIR / "hbt" / "hbt.nodes_stations_hbt.csv"
+    data = load_data(filename)
+    data = data[data["sn"] == device_id]
+    data = data[data["createdts"] >= startdate]
+    raw_data["hbt.nodes_stations_hbt"] = data.copy()
+
+    # Speed (UL, DL)
+    filename = DATA_DIR / "diag" / "diag.nodes_speedtest_diag.csv"
+    data = load_data(filename)
+    data = data[data["sn"] == device_id]
+    data = data[data["createdts"] >= startdate]
+    raw_data["diag.nodes_speedtest_diag"] = data.copy()
+
+    
+    
