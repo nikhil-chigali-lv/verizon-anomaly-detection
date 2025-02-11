05dd282bc1f59f971407b18588679d8c61b2f73e
nikhil-chigali-lv <nikhil.chigali@latentview.com>
2025-02-11 16:42:59 -0600

    Initial implementation of first half of the KPI features

--- feature_engineering/extract_from_csv.py
+++ feature_engineering/extract_from_csv.py
@@ -55,13 +55,33 @@ def extract_data(device_id: str, startdate: str):
     #   If the difference between the two is greater than 5, then "Mobile" else "Stationary"
     # Phy Rate {DownlinkPhyRate, MaxDownlinkPhyRate, UplinkPhyRate, MaxUplinkPhyRate} ✅
     #   hbt.nodes_stations_hbt.[stations_tx_link_rate, stations_max_tx_phy_rate, stations_link_rate, stations_max_rx_phy_rate]
+    # Num Clients Connected ✅
+    #   hbt.group_nodes_hbt.tplg_node_sta_num
+    # Num Access Points ✅
+    #   Number of Unique `hbt.group_nodes_hbt.tplg_node_device_mac`
+    # Mean Clients Per AP ✅ ❓
+    #  for each `AP = hbt.group_nodes_hbt.tplg_node_device_mac`, compute the number of unique `hbt.nodes_stations_hbt.stations_mac` with `hbt.nodes_stations_hbt.parent_id = AP` and take the mean across all APs
+    # Channel Switches ✅
+    #   Count `diag.nodes_acs_channelchange_diag.acs_bid` where acs_primary_ch != acs_prev_ch
+    # Wifi Associations ✅
+    #   "Disassociation" if `evt.nodes_station_evt.event_type` = 3. "Association" if `evt.nodes_station_evt.event_type` = 4
+    # SON status (On/Off) ✅
+    #   hbt.group_nodes_hbt.tplg_node_son
+    # SON Steer Success Status (Success/Failure) ✅
+    #   if `evt.stations_mesh_evt.event_type` = (2 or 3) and `evt.stations_mesh_evt.event_action` = 0 then "Failure", elif
+    #   `evt.stations_mesh_evt.event_type` = (2 or 3) and `evt.stations_mesh_evt.event_action` = 1 then "Success"
+    # Num Device Power Cycles ✅
+    #   Number of `evt.nodes_events_evt.event_type` in [1,8,12,13]
+    # Latency ✅
+    #   diag.nodes_speedtest_diag.ookla_result.Latency
 
-    raw_data = {}
+
     try:
         startdate = datetime.strptime(startdate, "%Y-%m-%d")
     except ValueError:
         logger.error("Invalid start date format. Use 'YYYY-MM-DD' format")
         sys.exit(1)
+    raw_data = {"timestamp": startdate, "device_id": device_id}
 
     # Data Used (UL, DL), Legacy Devices Exist, Signal Strength, Phy Rate
     filename = DATA_DIR / "hbt" / "hbt.nodes_stations_hbt.csv"
@@ -70,12 +90,48 @@ def extract_data(device_id: str, startdate: str):
     data = data[data["createdts"] >= startdate]
     raw_data["hbt.nodes_stations_hbt"] = data.copy()
 
-    # Speed (UL, DL)
+    # Speed (UL, DL), Latency
     filename = DATA_DIR / "diag" / "diag.nodes_speedtest_diag.csv"
     data = load_data(filename)
     data = data[data["sn"] == device_id]
     data = data[data["createdts"] >= startdate]
     raw_data["diag.nodes_speedtest_diag"] = data.copy()
 
-    
-    
+    # Num Clients Connected, Num Access Points, Mean Clients Per AP, SON Status
+    filename = DATA_DIR / "hbt" / "hbt.group_nodes_hbt.csv"
+    data = load_data(filename)
+    data = data[data["sn"] == device_id]
+    data = data[data["createdts"] >= startdate]
+    raw_data["hbt.group_nodes_hbt"] = data.copy()
+
+    # Channel Switches
+    filename = DATA_DIR / "diag" / "diag.nodes_acs_channelchange_diag.csv"
+    data = load_data(filename)
+    data = data[data["sn"] == device_id]
+    data = data[data["createdts"] >= startdate]
+    raw_data["diag.nodes_acs_channelchange_diag"] = data.copy()
+
+    # Wifi Associations
+    filename = DATA_DIR / "evt" / "evt.nodes_station_evt.csv"
+    data = load_data(filename)
+    data = data[data["sn"] == device_id]
+    data = data[data["createdts"] >= startdate]
+    raw_data["evt.nodes_station_evt"] = data.copy()
+
+    # SON Steer Success Status
+    filename = DATA_DIR / "evt" / "evt.stations_mesh_evt.csv"
+    data = load_data(filename)
+    data = data[data["sn"] == device_id]
+    data = data[data["createdts"] >= startdate]
+    raw_data["evt.stations_mesh_evt"] = data.copy()
+
+    # Num Device Power Cycles
+    filename = DATA_DIR / "evt" / "evt.nodes_events_evt.csv"
+    data = load_data(filename)
+    data = data[data["sn"] == device_id]
+    data = data[data["createdts"] >= startdate]
+    raw_data["evt.nodes_events_evt"] = data.copy()
+
+    return raw_data
+
+
--- feature_engineering/transform_features.py
+++ feature_engineering/transform_features.py
@@ -1,42 +1,161 @@
+## Assumption: the timestamps in hbt, evt, and diag data are synced.
+## TODO: Sync the data points based on the timestamps
+
 import numpy as np
 import pandas as pd
+from extract_from_csv import extract_data
 
-featureList = [
-    "DataUsed_UL",
-    "DataUsed_DL",
-    "Speed_UL",
-    "Speed_DL",
-    "LegacyDevicesExist",
-    "StationaryVsMobile",
-    "WifiScores_top3Devices", 
-    "PhyRate",
-    "SignalStrength",
-    "NumClientsConnected",
-    "NumAccessPoints",
-    "MeanClientsPerAP",
-    "ChannelSwitches",
-    "WifiAssociations",
-    "SON_status",
-    "SON_SteerSuccessStatus",
-    "NumDevicePowerCycles",
-    "KeyProcessCrashes",
-    "Latency",
-    "HomeWifiScore",
-    "HighSteeringCount",
-    "HomesWithWifiCongestion",
-    "PercentageOfDataOn5G",
-    "ClientBandwidthUsed",
-    "AirtimeCongestion",
-    "ChannelOfOperation",
-    "SON_PerClientSteer",
-    "ActivePhyRate",
-    "SuddenDropOfConnection",
-    "RebootPerformedViaCustomer",
-    "StationName",
-    "NumNeighborsOnSameChannel",
-    "NumOverallNeighbors",
-    "NumNeighborsOnAdjChannel",
-    "NumIPChanges",
-    "ExtendedBackhaulRange",
-    "EthernetAndNot1Gig"
-]
+## Features to generate
+# Data used - upload ✅
+#   hbt.nodes_stations_hbt.stations_bs
+# Data used - download ✅
+#   hbt.nodes_stations_hbt.stations_br
+# Speed - upload ✅
+#   diag.nodes_speedtest_diag.ookla_result.UploadSpeed
+# Speed - download ✅
+#   diag.nodes_speedtest_diag.ookla_result.DownloadSpeed
+# Legacy devices exist ✅
+#   if hbt.nodes_stations_hbt.stations_mode in [0,1,2,3] then "Yes" else "No"
+# Signal Strength ✅
+#   hbt.nodes_stations_hbt.stations_signal_strength
+# Stationary vs Mobile ✅ ❓
+#   Compute "90th Percentile" and "Median" of `Signal Strength`
+#   If the difference between the two is greater than 5, then "Mobile" else "Stationary"
+# Phy Rate {DownlinkPhyRate, MaxDownlinkPhyRate, UplinkPhyRate, MaxUplinkPhyRate} ✅
+#   hbt.nodes_stations_hbt.[stations_tx_link_rate, stations_max_tx_phy_rate, stations_link_rate, stations_max_rx_phy_rate]
+# Num Clients Connected ✅
+#   hbt.group_nodes_hbt.tplg_node_sta_num
+# Num Access Points ✅
+#   Number of Unique `hbt.group_nodes_hbt.tplg_node_device_mac`
+# Mean Clients Per AP ✅ ❓
+#  for each `AP = hbt.group_nodes_hbt.tplg_node_device_mac`, compute the number of unique `hbt.nodes_stations_hbt.stations_mac` with `hbt.nodes_stations_hbt.parent_id = AP` and take the mean across all APs
+# Channel Switches ✅
+#   Count `diag.nodes_acs_channelchange_diag.acs_bid` where acs_primary_ch != acs_prev_ch# Wifi Associations ✅
+#   "Disassociation" if `evt.nodes_station_evt.event_type` = 3. "Association" if `evt.nodes_station_evt.event_type` = 4
+# SON status (On/Off) ✅
+#   hbt.group_nodes_hbt.tplg_node_son
+# SON Steer Success Status (Success/Failure) ✅
+#   if `evt.stations_mesh_evt.event_type` = (2 or 3) and `evt.stations_mesh_evt.event_action` = 0 then "Failure", elif
+#   `evt.stations_mesh_evt.event_type` = (2 or 3) and `evt.stations_mesh_evt.event_action` = 1 then "Success"
+# Num Device Power Cycles ✅
+#   Number of `evt.nodes_events_evt.event_type` in [1,8,12,13]
+# Latency ✅
+#   diag.nodes_speedtest_diag.ookla_result.Latency
+
+
+def generate_features(data: pd.DataFrame):
+    features = pd.DataFrame()
+
+    # Data used - upload
+    features["upload_data"] = data["hbt.nodes_stations_hbt"]["stations_bs"]
+
+    # Data used - download
+    features["download_data"] = data["hbt.nodes_stations_hbt"]["stations_br"]
+
+    # Speed - upload
+    features["upload_speed"] = data["diag.nodes_speedtest_diag"]["ookla_result"][
+        "UploadSpeed"
+    ]
+
+    # Speed - download
+    features["download_speed"] = data["diag.nodes_speedtest_diag"]["ookla_result"][
+        "DownloadSpeed"
+    ]
+
+    # Legacy devices exist
+    features["legacy_devices_exist"] = np.where(
+        data["hbt.nodes_stations_hbt"]["stations_mode"].isin([0, 1, 2, 3]), "Yes", "No"
+    )
+
+    # Signal Strength
+    features["signal_strength"] = data["hbt.nodes_stations_hbt"][
+        "stations_signal_strength"
+    ]
+
+    # Stationary vs Mobile
+    signal_strength_90th = data["hbt.nodes_stations_hbt"][
+        "stations_signal_strength"
+    ].quantile(0.9)
+    signal_strength_median = data["hbt.nodes_stations_hbt"][
+        "stations_signal_strength"
+    ].median()
+    features["stationary_vs_mobile"] = np.where(
+        (signal_strength_90th - signal_strength_median) > 5, "Mobile", "Stationary"
+    )
+
+    # Phy Rate
+    features["downlink_phy_rate"] = data["hbt.nodes_stations_hbt"][
+        "stations_tx_link_rate"
+    ]
+    features["max_downlink_phy_rate"] = data["hbt.nodes_stations_hbt"][
+        "stations_max_tx_phy_rate"
+    ]
+    features["uplink_phy_rate"] = data["hbt.nodes_stations_hbt"]["stations_link_rate"]
+    features["max_uplink_phy_rate"] = data["hbt.nodes_stations_hbt"][
+        "stations_max_rx_phy_rate"
+    ]
+
+    # Num Clients Connected
+    features["num_clients_connected"] = data["hbt.group_nodes_hbt"]["tplg_node_sta_num"]
+
+    # Num Access Points
+    features["num_access_points"] = data["hbt.group_nodes_hbt"][
+        "tplg_node_device_mac"
+    ].nunique()
+
+    # Mean Clients Per AP
+    ap_clients = (
+        data["hbt.group_nodes_hbt"]
+        .groupby("tplg_node_device_mac")["stations_mac"]
+        .nunique()
+    )
+    features["mean_clients_per_ap"] = ap_clients.mean()
+
+    # Channel Switches
+    features["channel_switches"] = (
+        data["diag.nodes_acs_channelchange_diag"]["acs_bid"]
+        .where(
+            data["diag.nodes_acs_channelchange_diag"]["acs_primary_ch"]
+            != data["diag.nodes_acs_channelchange_diag"]["acs_prev_ch"]
+        )
+        .count()
+    )
+
+    # Wifi Associations
+    features["wifi_associations"] = np.where(
+        data["evt.nodes_station_evt"]["event_type"] == 3,
+        "Disassociation",
+        np.where(
+            data["evt.nodes_station_evt"]["event_type"] == 4, "Association", np.nan
+        ),
+    )
+
+    # SON status (On/Off)
+    features["son_status"] = data["hbt.group_nodes_hbt"]["tplg_node_son"]
+
+    # SON Steer Success Status (Success/Failure)
+    features["son_steer_success_status"] = np.where(
+        (data["evt.stations_mesh_evt"]["event_type"].isin([2, 3]))
+        & (data["evt.stations_mesh_evt"]["event_action"] == 0),
+        "Failure",
+        np.where(
+            (data["evt.stations_mesh_evt"]["event_type"].isin([2, 3]))
+            & (data["evt.stations_mesh_evt"]["event_action"] == 1),
+            "Success",
+            np.nan,
+        ),
+    )
+
+    # Num Device Power Cycles
+    features["num_device_power_cycles"] = (
+        data["evt.nodes_events_evt"]["event_type"].isin([1, 8, 12, 13]).sum()
+    )
+
+    # Latency
+    features["latency"] = data["diag.nodes_speedtest_diag"]["ookla_result"]["Latency"]
+
+    # Timestamp and device_id
+    features["timestamp"] = data["hbt.nodes_stations_hbt"]["createdts"]
+    features["device_id"] = data["hbt.nodes_stations_hbt"]["sn"]
+
+    return features
